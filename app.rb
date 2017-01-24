04f348ddfa5731bc78fc53e45e425fc688bdeb08
Linh Nguyen My <lnguyenmy1@gmail.com>
2017-01-24 23:55:45 +0000

    Updated app.rb

--- app.rb
+++ app.rb
@@ -2,7 +2,12 @@ require "sinatra"
 require_relative "./get_links"
 
 get "/getlinks" do
-  get_links = GetLinks.new
-  result = get_links.find_twitter(params["link_input"])
-  erb index, :locals => { :result => result }
+  url = params["link_input"]
+    if url
+      get_links = GetLinks.new
+      result = get_links.find_twitter(url)
+    else
+      result = []
+    end
+  erb :index, :locals => { :result => result }
 end
